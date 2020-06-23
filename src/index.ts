53c96faccc633e057441e3892634ae1e140aa68d
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2020-06-22 21:07:46 -0400

    types: export a bunch of types

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,13 @@ export { useEvents } from './useEvents'
 export { createOple, createClass } from './createOple'
 export { withOple, getOple, expectOple } from './global'
 export { setEffect, restoreEffects } from './Ople'
+export {
+  OpleInitFn,
+  OpleSetFn,
+  OpleEmitFn,
+  OpleObject,
+  ReadonlyOple,
+  ReadonlyOpleObject,
+} from './types'
 
 export * from 'wana'
