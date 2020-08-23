e5d2bddfa8aa8263a21efca53dc489058f814370
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2020-08-23 11:30:27 -0400

    feat(createClass): accept an optional super class
    which must extend the Ople class

--- src/createClass.ts
+++ src/createClass.ts
@@ -2,6 +2,7 @@ import { Ople } from './Ople'
 import { initOple } from './initOple'
 import { ReadonlyOpleObject, OpleInitFn } from './types'
 import { UnknownProps } from 'types'
+import { Class } from 'is'
 
 export function createClass<
   State extends object = UnknownProps,
@@ -9,8 +10,12 @@ export function createClass<
   Args extends ReadonlyArray<any> = any[]
 >(
   name: string,
-  getInit: (...args: Args) => OpleInitFn<State, Events>
+  getInit: (...args: Args) => OpleInitFn<State, Events>,
+  Super: Class = Ople
 ): new (...args: Args) => ReadonlyOpleObject<State, Events> {
+  if (!(Super.prototype instanceof Ople)) {
+    throw TypeError('Super class must extend the Ople class')
+  }
   const ctr = new Function(
     'initOple',
     'Ople',
@@ -21,7 +26,7 @@ export function createClass<
          getInit.apply(null, arguments)
        )
      }`
-  )(initOple, Ople, getInit)
-  Object.setPrototypeOf(ctr.prototype, Ople.prototype)
+  )(initOple, Super, getInit)
+  Object.setPrototypeOf(ctr.prototype, Super.prototype)
   return ctr as any
 }
